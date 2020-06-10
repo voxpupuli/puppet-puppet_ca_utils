1ef54f8887f3348c5581c1ed150be4a184202d08
Reid Vandewiele <reid@puppet.com>
2020-06-10 23:01:04 +0000

    Variable name change crl => crt
    For clarity

--- tasks/sync_ca.rb
+++ tasks/sync_ca.rb
@@ -9,14 +9,14 @@ require_relative '../../ruby_task_helper/files/task_helper.rb'
 # Example task that is based on the ruby_task_helper
 class SyncCAContent < TaskHelper
   def task(name: nil, **kwargs)
-    all_crls = []
+    all_crts = []
 
     # Get remote ca
     cmd = ['curl', '-k', '-s', "https://#{kwargs[:ca_hostname]}:8140/puppet-ca/v1/certificate/ca"]
     stdout, stderr, status = Open3.capture3(*cmd)
     raise Puppet::Error, _("{ stderr: #{stderr} }") if status != 0
 
-    # Read current ca_crl file
+    # Read current ca_crt file
     local_cas_all = File.read('/etc/puppetlabs/puppet/ssl/certs/ca.pem')
 
     # Split up certs
@@ -26,26 +26,26 @@ class SyncCAContent < TaskHelper
 
     # Get all certs as ruby CRL objects
     local_ca_strings.each do |local_ca|
-      all_crls << OpenSSL::X509::Certificate.new("#{local_ca}\n-----END CERTIFICATE-----\n-")
+      all_crts << OpenSSL::X509::Certificate.new("#{local_ca}\n-----END CERTIFICATE-----\n-")
     end
 
-    remote_crls_strings = stdout.split(delimeter)
+    remote_crts_strings = stdout.split(delimeter)
 
     # Get all certs as ruby CRL objects
-    remote_crls_strings.each do |remote_crl|
-      all_crls << OpenSSL::X509::Certificate.new("#{remote_crl}\n-----END CERTIFICATE-----\n")
+    remote_crts_strings.each do |remote_crt|
+      all_crts << OpenSSL::X509::Certificate.new("#{remote_crt}\n-----END CERTIFICATE-----\n")
     end
 
     # Remove duplicates
-    all_crls.uniq! { |crl| crl.to_pem }
+    all_crts.uniq! { |crt| crt.to_pem }
 
     # # Get back pem representations
-    # unique_crls = all_crls.select { |crl|
-    #   crl.to_pem()
+    # unique_crts = all_crts.select { |crt|
+    #   crt.to_pem()
     # }
 
     # # Rebuild CRL and write it back
-    new_ca = all_crls.join('')
+    new_ca = all_crts.join('')
 
     # puts "New ca #{new_ca}"
 
