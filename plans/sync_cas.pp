e526503edf8ab19538850b163303c4fea124957b
Bryan Belanger <bryan@fervid.us>
2020-06-24 13:34:22 -0400

    Remove apply_prep

--- plans/sync_cas.pp
+++ plans/sync_cas.pp
@@ -13,55 +13,55 @@ plan manage_ca_file::sync_cas (
 
   out::message("Bundle ${api_ca_data['crl_bundle']}")
 
-  if ($crl_bundle == 'full') {
-    $full_crl_bundle = run_task('manage_ca_file::get_ca_crl', $ca_targets).map |$r| {
-      $r['ca_crl']
-    }.manage_ca_file::merge_crl_bundles()
-  }
-  else { # $crl_bundle == 'api'
-    $full_crl_bundle = $api_ca_data['crl_bundle']
-  }
+  # if ($crl_bundle == 'full') {
+  #   $full_crl_bundle = run_task('manage_ca_file::get_ca_crl', $ca_targets).map |$r| {
+  #     $r['ca_crl']
+  #   }.manage_ca_file::merge_crl_bundles()
+  # }
+  # else { # $crl_bundle == 'api'
+  $full_crl_bundle = $api_ca_data['crl_bundle']
+  # }
 
-  $ordered_pem_bundles = {
-    'ca_crt'    => manage_ca_file::ordered_ca_bundles($api_ca_data['peer_certs'], $api_ca_data['ca_bundle']),
-    'ca_crl'    => manage_ca_file::ordered_crl_bundles($api_ca_data['peer_certs'], $full_crl_bundle),
-    'infra_crl' => manage_ca_file::ordered_crl_bundles($api_ca_data['peer_certs'], $api_ca_data['crl_bundle']),
-  }
+  # $ordered_pem_bundles = {
+  #   'ca_crt'    => manage_ca_file::ordered_ca_bundles($api_ca_data['peer_certs'], $api_ca_data['ca_bundle']),
+  #   'ca_crl'    => manage_ca_file::ordered_crl_bundles($api_ca_data['peer_certs'], $full_crl_bundle),
+  #   'infra_crl' => manage_ca_file::ordered_crl_bundles($api_ca_data['peer_certs'], $api_ca_data['crl_bundle']),
+  # }
 
-  # We will use the 'name' var in the apply block below
-  $update_targets.each |$target| {
-    $target.set_var('hostname', $target.name)
-  }
+  # # We will use the 'name' var in the apply block below
+  # $update_targets.each |$target| {
+  #   $target.set_var('hostname', $target.name)
+  # }
 
   # apply_prep($update_targets)
 
   # Note that there is a race condition here around the CRL.
   # See https://tickets.puppetlabs.com/browse/SERVER-2550
   apply($update_targets) {
-    File {
-      ensure => file,
-      owner  => 'pe-puppet',
-      group  => 'pe-puppet',
-      notify => $restart_puppetserver ? {
-        true  => Service['pe-puppetserver'],
-        false => undef,
-      },
-    }
+    # File {
+    #   ensure => file,
+    #   owner  => 'pe-puppet',
+    #   group  => 'pe-puppet',
+    #   notify => $restart_puppetserver ? {
+    #     true  => Service['pe-puppetserver'],
+    #     false => undef,
+    #   },
+    # }
 
-    file { '/etc/puppetlabs/puppet/ssl/certs/ca.pem':
-      content => $ordered_pem_bundles['ca_crt'][$hostname],
-    }
+    # file { '/etc/puppetlabs/puppet/ssl/certs/ca.pem':
+    #   content => $ordered_pem_bundles['ca_crt'][$hostname],
+    # }
 
-    file { '/etc/puppetlabs/puppet/ssl/ca/ca_crl.pem':
-      content => $ordered_pem_bundles['ca_crl'][$hostname],
-    }
+    # file { '/etc/puppetlabs/puppet/ssl/ca/ca_crl.pem':
+    #   content => $ordered_pem_bundles['ca_crl'][$hostname],
+    # }
 
-    file { '/etc/puppetlabs/puppet/ssl/ca/infra_crl.pem':
-      content => $ordered_pem_bundles['infra_crl'][$hostname],
-    }
+    # file { '/etc/puppetlabs/puppet/ssl/ca/infra_crl.pem':
+    #   content => $ordered_pem_bundles['infra_crl'][$hostname],
+    # }
 
-    # Question: does Puppet Server need reloading?
-    service { 'pe-puppetserver': }
+    # # Question: does Puppet Server need reloading?
+    # service { 'pe-puppetserver': }
   }
 
   # Note: agents and compilers will recieve the updated CA bundle and CRL through normal
